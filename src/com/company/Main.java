a806378cb0913811f77f65d734d13e79a8b5bc74
Alexandr <asentsov@inno.tech>
2023-02-20 01:53:22 +0300

    add code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //длина надписи
+        int inscriptionLength = 346;
+
+        //вводим длинну забора
+        System.out.println("Введите длинну забора");
+        Scanner scanner = new Scanner(System.in);
+        int fenceLength = scanner.nextInt();
+
+        if (fenceLength >= inscriptionLength) {
+            System.out.println("Надпись поместится");
+        } else {
+            System.out.println("Надпись не поместится");
+        }
+    }
 }
